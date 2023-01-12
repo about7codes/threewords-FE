be8653feed4439a59d16a1d4edb5072d80e79a5a
sanaullah <sanaullahhaifz@gmail.com>
2023-01-12 11:50:35 +0400

    added google analytics

--- components/HeaderInfo/HeaderInfo.tsx
+++ components/HeaderInfo/HeaderInfo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Script from "next/script";
 
 interface IHeaderInfoProps {
   title: string;
@@ -7,11 +8,27 @@ interface IHeaderInfoProps {
 
 const HeaderInfo = ({ title }: IHeaderInfoProps) => {
   return (
-    <Head>
-      <title>{title} | Threewords App</title>
-      <meta name="description" content="Threewords App next js app" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>{title} | Threewords App</title>
+        <meta name="description" content="Threewords App next js app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-9Y0G07RD5N"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-9Y0G07RD5N');
+          `}
+      </Script>
+    </>
   );
 };
 
